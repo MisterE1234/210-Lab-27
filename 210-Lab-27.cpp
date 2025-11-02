80a3a140bed66bb336d24d2d61eb917aed851fe5
MisterE1234 <MisterE1234@outlook.com>
2025-11-02 13:28:36 -0800

    2nd Commit

--- 210-Lab-27.cpp
+++ 210-Lab-27.cpp
@@ -11,6 +11,10 @@ using namespace std;
 int menu();
 
 int main() {
+
+    string searchKey;
+    bool exitLoop = false;
+    int choice;
     // declarations
     map<string, tuple <int, string, string>> villagerFriend; // map of villager Friendship levels (name, tuple (friendship level, species, catchphrase))
 
@@ -19,6 +23,40 @@ int main() {
     villagerFriend["Audie"] = {8, "Human", "Cowabunga!"};
     villagerFriend["Raymond"] = {5, "Mutant", "What's up?"};
     villagerFriend.insert({"Marshal", {2, "Vampire", "Coo coo ca-choo!"}});
+    
+    while(!exitLoop){
+
+        
+
+        switch(menu()){
+            case 1: //Increase Friendship:
+                cout << "Enter the first name of the villager to increase friendship:";
+                cin >> searchKey;
+                auto it = villagerFriend.find(searchKey);
+                
+                if (it != villagerFriend.end()) {  // the iterator points to beyond the end of the map
+                                       // if searchKey is not found
+                    cout << "\nFound " << searchKey << " [ ";
+        
+                    //listing the friendship level
+                    cout << get<0>(it->second) << ", ";
+                    //listing the species
+                    cout << get<1>(it->second) << ", ";
+                    //listing the catchphrase
+                    cout << get<2>(it->second) << "]" << endl;
+            
+                    cout << endl;
+            } else
+                cout << endl << searchKey << " not found." << endl;
+
+            
+
+
+
+
+
+
+        }
 
     // access the map using a range-based for loop
     cout << "Villagers, their friendship level,species, and favorite catchphrases (range-based for loop):" << endl;
@@ -50,7 +88,7 @@ int main() {
     villagerFriend.erase("Raymond");
 
     // search for an element using .find() to avoid errors
-    string searchKey = "Audie";
+    searchKey = "Audie";
     auto it = villagerFriend.find(searchKey);
     if (it != villagerFriend.end()) {  // the iterator points to beyond the end of the map
                                        // if searchKey is not found
@@ -75,10 +113,14 @@ int main() {
     return 0;
 }
 
+//menu() display the menu and get user choice:
+//Requires: nothing
+//Return: an int value representing the user's menu choice
 int menu(){
     bool valid = false;
     int choice;
 
+    //a while loop to validate user input:
     while(!valid){
     cout << "1. Increase Friendship\n" << "2. Decrease Friendship\n"
     << "3. Search for Villager\n" << "4. Exit\n" << endl;
@@ -89,6 +131,7 @@ int menu(){
     cout << "Enter choice (integer 1-4):";
     cin >> choice;
 
+    //If the user enters a valid choice (1-4), set valid to true to exit the loop, if not restart the loop:
     if(choice == 1 || choice == 2 || choice == 3 || choice == 4){
         valid = true;
     }
