4347a800d01d5b1f849e9d96602706fde864989f
MisterE1234 <MisterE1234@outlook.com>
2025-11-01 21:01:08 -0700

    3rd Commit

--- 210-Lab-27.cpp
+++ 210-Lab-27.cpp
@@ -14,20 +14,22 @@ int main() {
     // note how the right-hand side of the assignment are the vector elements
     villagerFriend["Audie"] = {8, "Cowabunga!"};
     villagerFriend["Raymond"] = {5, "What's up?"};
-    villagerFriend.insert({"Marshal", {"2", "Coo coo ca-choo!"}});
+    villagerFriend.insert({"Marshal", {2, "Coo coo ca-choo!"}});
 
     // access the map using a range-based for loop
-    cout << "Villagers and their favorite colors (range-based for loop):" << endl;
+    cout << "Villagers, their friendship level, and their favorite catchphrases (range-based for loop):" << endl;
     for (auto pair : villagerFriend) {
         cout << pair.first << ": ";
-        for (auto color : pair.second)
-            cout << color << " ";
+        for(auto level : get<0>(pair.second))
+            cout << "Friendship Level: " << level << ", ";
+        for (auto catchphrase : get<1>(pair.second))
+            cout << "\"" << catchphrase << "\" ";
         cout << endl;
     }
 
     // access the map using iterators
     cout << "\nVillagers and their favorite colors (iterators):" << endl;
-    for (map<string, tuple<string>>::iterator it = villagerFriend.begin(); 
+    for (map<string, tuple<int, string>>::iterator it = villagerFriend.begin(); 
                                                it != villagerFriend.end(); ++it) {
         cout << it->first << ": ";
         for (auto color : it->second) {
@@ -44,7 +46,7 @@ int main() {
     auto it = villagerFriend.find(searchKey);
     if (it != villagerFriend.end()) {  // the iterator points to beyond the end of the map
                                        // if searchKey is not found
-        cout << "\nFound " << searchKey << "'s favorite colors: ";
+        cout << "\nFound " << searchKey << "'s favorite catchphrase: ";
         for (auto color : it->second)  // range loop to traverse the value/vector
             cout << color << " ";
         cout << endl;
